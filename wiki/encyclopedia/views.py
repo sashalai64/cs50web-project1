cfb9ea55c253a55178c7c7328d7b69f577602618
sashalai64 <91836741+sashalai64@users.noreply.github.com>
2024-04-23 01:49:16 +0800

    Add entry view

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+
+    if content is None:
+        return render(request, "encyclopedia/error.html")
+    
+    #convert text to HTML
+    markdown_text = markdown2.markdown(content)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": markdown_text
+    })
+
+    
