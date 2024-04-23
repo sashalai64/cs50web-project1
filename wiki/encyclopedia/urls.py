329fd542da1449c254dbb2ca282868f3785d9171
sashalai64 <91836741+sashalai64@users.noreply.github.com>
2024-04-24 02:18:30 +0800

    Add link to entries

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name = "entry")
+    path("<str:title>", views.entry, name = "title"),
+    
 ]
